c87784c60fd77f17a3b35d1544c2c6c73fc4df45
James Sellers <32497563+Jsellers1977@users.noreply.github.com>
2018-05-30 20:08:10 -0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -18,7 +18,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "james@thesocialmovement.com";
+        $recipient = "jsellers21@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
