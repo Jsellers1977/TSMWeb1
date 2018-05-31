5dddcce275d95bfcfc3bb29e7286c76a73111868
James Sellers <32497563+Jsellers1977@users.noreply.github.com>
2018-05-31 10:28:17 -0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -1,12 +1,12 @@
 <?php
 
     // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_GET["name"]));
+        $name = strip_tags(trim($_POST["name"]));
 				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_GET["email"]), FILTER_SANITIZE_EMAIL);
-        $message = trim($_GET["message"]);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
 
         // Check that data was sent to the mailer.
         if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
